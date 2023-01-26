220b9c029cd5ba42f97a6aef9c4908f6609fc4e2
xiaoua <xiaoua@localhost>
2023-01-26 07:38:16 -0700

    ADD structure

--- apps/bench/netbench_udp.cc
+++ apps/bench/netbench_udp.cc
@@ -27,6 +27,8 @@ extern "C" {
 #include <algorithm>
 #include <numeric>
 #include <random>
+#include <string>
+
 
 namespace {
 
@@ -48,399 +50,257 @@ uint64_t n;
 // the mean service time in us.
 double st;
 
-
-
-int32_t idx; // idx in cluster, client is -1.
+const uint32_t STATUS_NORMAL = 0;
+const uint32_t STATUS_VIEW_CHANGE = 1;
+const uint32_t STATUS_RECOVERING = 2;
 
 // Paxos-state
+int32_t idx;
 uint32_t view;
+uint32_t status;
 
 
 // ------------------------------------ server-side code ------------------------------------
-void ServerWorker(rt::UdpConn *c) {
-  union {
-    unsigned char buf[rt::UdpConn::kMaxPayloadSize];
-    payload p;
-  };
-  std::unique_ptr<FakeWorker> w(FakeWorkerFactory("stridedmem:3200:64"));
-  if (unlikely(w == nullptr)) panic("couldn't create worker");
 
-  while (true) {
-    // Receive a network response.
-    ssize_t ret = c->Read(&buf, sizeof(buf));
-    if (ret <= 0 || ret > static_cast<ssize_t>(sizeof(buf))) {
-      if (ret == 0) break;
-      panic("udp read failed, ret = %ld", ret);
+bool AmLeader() {
+	return view == 0;
+}
+
+void HandleRequest(const netaddr &remote,
+                   const specpaxos::vr::proto::RequestMessage &msg) {
+	if (status != STATUS_NORMAL) { // don't handle request.
+        puts("Ignoring request due to abnormal status");
+		exit(-1);
+        return;
+    }
+	
+	if (!AmLeader()) { // only leader should handle request.
+        puts("Ignoring request because I'm not the leader");
+        exit(-1);
+        return;
     }
 
-    // Determine if the connection is being killed.
-    if (unlikely(p.tag == kKill)) {
-      c->Shutdown();
-      break;
+	// now I'm the leader.
+}
+
+void HandlePrepare(const netaddr &remote,
+				   const specpaxos::vr::proto::PrepareMessage &msg) {
+    if (status != STATUS_NORMAL) { // no interaction.
+        puts("Ignoring PREPARE due to abnormal status");
+		exit(-1);
+        return;
+    }
+    
+    if (msg.view() < view) { // hear a stale  message, we shouldn't respond to that.
+        puts("Ignoring PREPARE due to stale view");
+		exit(-1);
+        return;
     }
 
-    // Perform fake work if requested.
-    if (p.workn != 0) w->Work(p.workn * 82.0);
+    if (msg.view() > view) {
+		puts("trigger view change! we laid behind.");
+		exit(-1);
+        return;
+	}
 
-    // Send a network request.
-    ssize_t sret = c->Write(&buf, ret);
-    if (sret != ret) {
-      if (sret == -EPIPE) break;
-      panic("udp write failed, ret = %ld", sret);
+    if (AmLeader()) { // leader shouldn't receive this message.
+        puts("Unexpected PREPARE: I'm the leader of this view");
+		exit(-1);
     }
-  }
+
+
+}
+
+void HandlePrepareOK(const netaddr &remote, 
+					 const specpaxos::vr::proto::PrepareOKMessage &msg) {
+    if (status != STATUS_NORMAL) {
+        puts("Ignoring PREPAREOK due to abnormal status");
+		exit(-1);
+        return;
+    }
+
+    if (msg.view() < view) {
+        puts("Ignoring PREPAREOK due to stale view");
+		exit(-1);
+        return;
+    }
+
+    if (msg.view() > view) {
+		puts("trigger view change! we laid behind.");
+		exit(-1);
+        return;
+    }
+
+    if (!AmLeader()) {
+        puts("Ignoring PREPAREOK because I'm not the leader");
+		exit(-1);
+        return;
+    }
+
+}
+
+void HandleCommit(const netaddr &remote,
+				  const specpaxos::vr::proto::CommitMessage &msg) {
+    if (status != STATUS_NORMAL) {
+        puts("Ignoring COMMIT due to abnormal status");
+		exit(-1);
+        return;
+    }
+    
+    if (msg.view() < view) {
+        puts("Ignoring COMMIT due to stale view");
+		exit(-1);
+        return;
+    }
+
+    if (msg.view() > view) {
+		puts("trigger view change! we laid behind.");
+		exit(-1);
+        return;
+    }
+
+    if (AmLeader()) {
+        puts("Unexpected COMMIT: I'm the leader of this view");
+		exit(-1);
+    }
+}
+
+void ReceiveMessage(const netaddr &remote, const std::string &type, const std::string &data) {
+    static specpaxos::vr::proto::RequestMessage request;
+    static specpaxos::vr::proto::PrepareMessage prepare;
+    static specpaxos::vr::proto::PrepareOKMessage prepareOK;
+    static specpaxos::vr::proto::CommitMessage commit;
+    
+    if (type == request.GetTypeName()) { // HandleRequest, the leader's duty.
+        request.ParseFromString(data);
+        HandleRequest(remote, request);
+    } else if (type == prepare.GetTypeName()) { // HandlePrepare, in backup replica.
+        prepare.ParseFromString(data);
+        HandlePrepare(remote, prepare);
+    } else if (type == prepareOK.GetTypeName()) { // HandlePrepareOK, the leader's duty.
+        prepareOK.ParseFromString(data);
+        HandlePrepareOK(remote, prepareOK);
+    } else if (type == commit.GetTypeName()) { // HandleCommit, in back replica.
+        commit.ParseFromString(data);
+        HandleCommit(remote, commit);
+    } else {
+		printf("Received unexpected message type in VR proto: %s\n", type.c_str());
+		fflush(stdout);
+		exit(-1);
+	}
+
+    // asd123www: add logic here if we have handle view-change.
 }
 
 // the main function of Server.
 void ServerHandler(void *arg) {
-  std::unique_ptr<rt::UdpConn> c(rt::UdpConn::Listen({0, kNetbenchPort}));
-  // std::unique_ptr<rt::UdpConn> c(rt::UdpConn::Dial(cltaddr, raddr));
-  if (unlikely(c == nullptr)) panic("couldn't listen for control connections");
+    std::unique_ptr<rt::UdpConn> c(rt::UdpConn::Listen({0, kNetbenchPort}));
+    if (unlikely(c == nullptr)) panic("couldn't listen for control connections");
 
-  char buf[10005];
-  while (true) {
-    netaddr raddr;
-    printf("Server receiving..\n");
-    ssize_t ret = c->ReadFrom(buf, 1e4, &raddr);
-    printf("Recieved %ld from %d\n", ret, raddr.ip);
-    ret = udp_send(buf, ret, srvaddr[0], cltaddr);
-    if (ret == -1) {
-      puts("Failed sending reply!");fflush(stdout);
-      exit(0);
-    }
-  }
-  return;
+	// initialize Paxos's state
+	view = 0;
+	status = STATUS_NORMAL;
+
+	// like event-driven, a loop pooling packets. 
+    char buf[10005];
+    while (true) {
+        netaddr raddr;
+        ssize_t ret = c->ReadFrom(buf, 1e4, &raddr);
+
+        char *payload = buf;
+        uint64_t typeLen = *(uint64_t *)payload;
+        payload = payload + sizeof(uint64_t);
+        std::string type(payload, typeLen);
+        std::string data(payload + typeLen, ret - sizeof(uint64_t) - typeLen);
+        ReceiveMessage(raddr, type, data);
+	}
+	puts("Quited from loop!");
+    return;
 }
-
-
-
-
-
-
 
 
 // ------------------------------------ client-side code ------------------------------------
-// Close the connection.
-void KillConn(rt::UdpConn *c)
-{
-  constexpr int kKillRetries = 10;
-  union {
-    unsigned char buf[32];
-    payload p;
-  };
-  p.tag = kKill;
-  for (int i = 0; i < kKillRetries; ++i)
-    udp_send(buf, sizeof(buf), c->LocalAddr(), c->RemoteAddr());
-}
-
-
-// Sending requests.
-std::vector<double> PoissonWorker(rt::UdpConn *c, double req_rate,
-                                  double service_time, rt::WaitGroup *starter)
-{
-  // Seed the random generator with the local port number.
-  std::mt19937 g(c->RemoteAddr().port);
-
-  // Create a packet transmit schedule.
-  std::vector<double> sched;
-  std::exponential_distribution<double> rd(1.0 / (1000000.0 / req_rate));
-  std::vector<double> tmp(n);
-  std::generate(tmp.begin(), tmp.end(), std::bind(rd, g));
-  sched.push_back(tmp[0]);
-  for (std::vector<double>::size_type j = 1; j < tmp.size(); ++j) {
-    tmp[j] += tmp[j - 1];
-    sched.push_back(static_cast<uint64_t>(tmp[j]));
-  }
-
-  // Create a fake work schedule.
-  std::vector<double> work(n);
-  std::exponential_distribution<double> wd(1.0 / service_time);
-  std::generate(work.begin(), work.end(), std::bind(wd, g));
-
-  // Reserve space to record results.
-  auto n = sched.size();
-  std::vector<double> timings;
-  timings.reserve(n);
-  std::vector<uint64_t> start_us(n);
-
-  // Start the receiver thread.
-  auto th = rt::Thread([&]{
-    union {
-      unsigned char rbuf[32] = {};
-      payload rp;
-    };
-
-    while (true) {
-     ssize_t ret = c->Read(rbuf, sizeof(rbuf));
-     if (ret != static_cast<ssize_t>(sizeof(rbuf))) {
-       if (ret == 0) break;
-       panic("udp read failed, ret = %ld", ret);
-     }
-
-     barrier();
-     uint64_t ts = microtime();
-     barrier();
-     timings.push_back(ts - start_us[rp.idx]);
-    }
-  });
-
-  // Initialize timing measurement data structures.
-  union {
-    unsigned char buf[32] = {};
-    payload p;
-  };
-
-  // Synchronized start of load generation.
-  starter->Done();
-  starter->Wait();
-
-  barrier();
-  uint64_t expstart = microtime();
-  barrier();
-
-  for (unsigned int i = 0; i < n; ++i) {
-    barrier();
-    uint64_t now = microtime();
-    barrier();
-    if (now - expstart < sched[i]) {
-      rt::Sleep(sched[i] - (now - expstart));
-      now = microtime();
-    }
-    if (now - expstart - sched[i] > kMaxCatchUpUS)
-      continue;
-
-    barrier();
-    start_us[i] = microtime();
-    barrier();
-
-    // Send a network request.
-    p.idx = i;
-    p.workn = work[i];
-    p.tag = 0;
-    ssize_t ret = udp_send(buf, sizeof(buf), c->LocalAddr(), c->RemoteAddr());
-    if (ret != static_cast<ssize_t>(sizeof(buf)))
-      panic("udp write failed, ret = %ld", ret);
-  }
-
-  c->Shutdown();
-  th.Join();
-
-  return timings;
-}
-
-
-
-
-// build udp connection, send request and get reply.
-std::vector<double> RunExperiment(double req_rate, double *reqs_per_sec) {
-  std::unique_ptr<rt::UdpConn> c(rt::UdpConn::Dial({0, 0}, raddr));
-  if (c == nullptr) panic("couldn't establish control connection");
-
-  // Send the control message.
-  nbench_req req = {kMagic, threads};
-  ssize_t ret = c->Write(&req, sizeof(req));
-  if (ret != sizeof(req)) panic("couldn't send control message");
-
-  // Receive the control response.
-  union {
-    nbench_resp resp;
-    char buf[rt::UdpConn::kMaxPayloadSize];
-  };
-  ret = c->Read(&resp, rt::UdpConn::kMaxPayloadSize);
-  if (ret < static_cast<ssize_t>(sizeof(nbench_resp)))
-    panic("failed to receive control response");
-  if (resp.magic != kMagic || resp.nports != threads)
-    panic("got back invalid control response");
-
-  // Create one UDP connection per thread.
-  std::vector<std::unique_ptr<rt::UdpConn>> conns;
-  for (int i = 0; i < threads; ++i) {
-    // create local address, because it's client, port number arbitrary.
-    std::unique_ptr<rt::UdpConn>
-      outc(rt::UdpConn::Dial(c->LocalAddr(), {raddr.ip, resp.ports[i]}));
-    if (unlikely(outc == nullptr)) panic("couldn't connect to raddr.");
-    conns.emplace_back(std::move(outc));
-  }
-
-  // Launch a worker thread for each connection.
-  rt::WaitGroup starter(threads + 1);
-  std::vector<rt::Thread> th;
-  std::unique_ptr<std::vector<double>> samples[threads];
-  for (int i = 0; i < threads; ++i) {
-    th.emplace_back(rt::Thread([&, i]{
-      auto v = PoissonWorker(conns[i].get(), req_rate / threads, st,
-                             &starter);
-      samples[i].reset(new std::vector<double>(std::move(v)));
-    }));
-  }
-
-  // Give the workers time to initialize, then start recording.
-  starter.Done();
-  starter.Wait();
-
-  // |--- start experiment duration timing ---|
-  barrier();
-  auto start = std::chrono::steady_clock::now();
-  barrier();
-
-  // Wait for the workers to finish.
-  for (auto& t: th)
-    t.Join();
-
-  // |--- end experiment duration timing ---|
-  barrier();
-  auto finish = std::chrono::steady_clock::now();
-  barrier();
-
-  // Close the connections.
-  for (auto& c: conns)
-    KillConn(c.get());
-
-  // Aggregate all the latency timings together.
-  uint64_t total = 0;
-  std::vector<double> timings;
-  for (int i = 0; i < threads; ++i) {
-    auto &v = *samples[i];
-    total += v.size();
-    if (v.size() <= kDiscardSamples * 2) panic("not enough samples");
-    v.erase(v.begin(), v.begin() + kDiscardSamples);
-    v.erase(v.end() - kDiscardSamples, v.end());
-    timings.insert(timings.end(), v.begin(), v.end());
-  }
-
-  // Report results.
-  double elapsed = std::chrono::duration_cast<sec>(finish - start).count();
-  *reqs_per_sec = static_cast<double>(total) / elapsed * 1000000;
-  return timings;
-}
-
-
-
-// One epoch, calculate tail latency.
-void DoExperiment(double req_rate) {
-  constexpr int kRounds = 1; // only one round.
-  std::vector<double> timings;
-  double reqs_per_sec = 0;
-  for (int i = 0; i < kRounds; i++) {
-    double tmp;
-    auto t = RunExperiment(req_rate, &tmp);
-    timings.insert(timings.end(), t.begin(), t.end());
-    reqs_per_sec += tmp;
-    rt::Sleep(500 * rt::kMilliseconds);
-  }
-  reqs_per_sec /= kRounds;
-
-  std::sort(timings.begin(), timings.end());
-  double sum = std::accumulate(timings.begin(), timings.end(), 0.0);
-  double mean = sum / timings.size();
-  double count = static_cast<double>(timings.size());
-  double p9 = timings[count * 0.9];
-  double p99 = timings[count * 0.99];
-  double p999 = timings[count * 0.999];
-  double p9999 = timings[count * 0.9999];
-  double min = timings[0];
-  double max = timings[timings.size() - 1];
-  std::cout << std::setprecision(2) << std::fixed
-            << "t: "       << threads
-            << " rps: "    << reqs_per_sec
-            << " n: "      << timings.size()
-            << " min: "    << min
-            << " mean: "   << mean
-            << " 90%: "    << p9
-            << " 99%: "    << p99
-            << " 99.9%: "  << p999
-            << " 99.99%: " << p9999
-            << " max: "    << max << std::endl;
-}
-
-
 
 void ClientHandler(void *arg) {
-  std::unique_ptr<rt::UdpConn> c(rt::UdpConn::Listen({0, kNetbenchPort}));
-  if (unlikely(c == nullptr)) panic("couldn't listen for control connections");
+	std::unique_ptr<rt::UdpConn> c(rt::UdpConn::Listen({0, kNetbenchPort}));
+	if (unlikely(c == nullptr)) panic("couldn't listen for control connections");
 
-  // a simplified client, need to add warmup in the future.
-  int cnt = 10;
-  char buf[3000];
-  while (--cnt) {
-    int len = rand() % 50 + 50;
-    printf("Sending: %d\n", len);
-    ssize_t ret = udp_send(buf, len, cltaddr, srvaddr[0]);
-    if (ret == -1) {
-      puts("Failed sending request!");fflush(stdout);
-      exit(0);
-    }
-    ret = c->ReadFrom(buf, 3e3, &raddr);
-    printf("Received: %ld\n", ret);
-    // received one request.
-  }
-  return;
+	// a simplified client, need to add warmup in the future.
+	int cnt = 10;
+	char buf[3000];
+	while (--cnt) {
+		int len = rand() % 50 + 50;
+		printf("Sending: %d\n", len);
+		ssize_t ret = udp_send(buf, len, cltaddr, srvaddr[0]);
+		if (ret == -1) {
+			puts("Failed sending request!");fflush(stdout);
+			exit(0);
+		}
+		ret = c->ReadFrom(buf, 3e3, &raddr);
+		printf("Received: %ld\n", ret);
+		// received one request.
+	}
+	return;
 }
 
 
 // ipv4: convert string to u32.
 int StringToAddr(const char *str, uint32_t *addr) {
-  uint8_t a, b, c, d;
+	uint8_t a, b, c, d;
 
-  if(sscanf(str, "%hhu.%hhu.%hhu.%hhu", &a, &b, &c, &d) != 4) {
-    puts("Failed in parsing ipv4 addr");
-    exit(-1);
-    return -EINVAL;
-  }
+	if(sscanf(str, "%hhu.%hhu.%hhu.%hhu", &a, &b, &c, &d) != 4) {
+		puts("Failed in parsing ipv4 addr");
+		exit(-1);
+		return -EINVAL;
+	}
 
-  *addr = MAKE_IP_ADDR(a, b, c, d);
-  return 0;
+	*addr = MAKE_IP_ADDR(a, b, c, d);
+	return 0;
 }
 
 } // anonymous namespace
 
 int main(int argc, char *argv[]) {
-  int ret;
+	int ret;
 
-  if (argc < 3) {
-    std::cerr << "usage: [cfg_file] [cmd] ..." << std::endl;
-    return -EINVAL;
-  }
+	if (argc < 3) {
+		std::cerr << "usage: [cfg_file] [cmd] ..." << std::endl;
+		return -EINVAL;
+	}
 
-  specpaxos::vr::proto::PrepareMessage prepare;
-  prepare.set_view(1);
+	// Setting cluster.
+	StringToAddr("10.10.1.2", &cltaddr.ip);
+	StringToAddr("10.10.1.3", &srvaddr[0].ip);
+	StringToAddr("10.10.1.4", &srvaddr[1].ip);
+	StringToAddr("10.10.1.1", &srvaddr[2].ip);
+	StringToAddr("10.10.1.6", &srvaddr[3].ip);
+	StringToAddr("10.10.1.5", &srvaddr[4].ip);
+	StringToAddr("10.10.1.7", &srvaddr[5].ip);
+	StringToAddr("10.10.1.8", &srvaddr[6].ip);
+	cltaddr.port = kNetbenchPort;
+	for (int i = 0; i < 7; ++i) srvaddr[i].port = kNetbenchPort;
 
-  printf("view: %d\n", prepare.view());
+	std::string cmd = argv[2];
+	if (cmd.compare("server") == 0) {
+		puts("I'm running server!");
+		idx = std::stoi(argv[3], nullptr, 0);
+		ret = runtime_init(argv[1], ServerHandler, NULL);
+		if (ret) {
+			printf("Server: failed to start runtime\n");
+			return ret;
+		}
+	} else if (cmd.compare("client") != 0) {
+		std::cerr << "invalid command: " << cmd << std::endl;
+		return -EINVAL;
+	}
 
-  // Setting cluster.
-  StringToAddr("10.10.1.2", &cltaddr.ip);
-  StringToAddr("10.10.1.3", &srvaddr[0].ip);
-  StringToAddr("10.10.1.4", &srvaddr[1].ip);
-  StringToAddr("10.10.1.1", &srvaddr[2].ip);
-  StringToAddr("10.10.1.6", &srvaddr[3].ip);
-  StringToAddr("10.10.1.5", &srvaddr[4].ip);
-  StringToAddr("10.10.1.7", &srvaddr[5].ip);
-  StringToAddr("10.10.1.8", &srvaddr[6].ip);
-  cltaddr.port = kNetbenchPort;
-  for (int i = 0; i < 7; ++i) srvaddr[i].port = kNetbenchPort;
-  view = 0;
+	ret = runtime_init(argv[1], ClientHandler, NULL);
+	if (ret) {
+		printf("failed to start runtime\n");
+		return ret;
+	}
 
-  std::string cmd = argv[2];
-  if (cmd.compare("server") == 0) {
-    puts("I'm running server!");
-    idx = std::stoi(argv[3], nullptr, 0);
-    ret = runtime_init(argv[1], ServerHandler, NULL);
-    if (ret) {
-      printf("Server: failed to start runtime\n");
-      return ret;
-    }
-  } else if (cmd.compare("client") != 0) {
-    std::cerr << "invalid command: " << cmd << std::endl;
-    return -EINVAL;
-  }
-
-  ret = runtime_init(argv[1], ClientHandler, NULL);
-  if (ret) {
-    printf("failed to start runtime\n");
-    return ret;
-  }
-
-  return 0;
+	return 0;
 }
 /*
 Compiling your code:
